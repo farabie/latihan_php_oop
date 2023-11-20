d8810d8122403798b0aa30051b1bdc1159dbd87b
Muhammad Farabie <farabie024@gmail.com>
2023-11-20 14:38:08 +0700

    Implementation Object

---
+++ 2.Object/Public/object_book.php
@@ -0,0 +1,9 @@
+<?php 
+
+require_once "2.Object/Data/book.php";
+
+$book = new Book();
+var_dump($book);
+
+
+?>
---
+++ 2.Object/Public/object_car.php
@@ -0,0 +1,8 @@
+<?php 
+
+require_once "2.Object/Data/car.php";
+
+$car = new Car();
+var_dump($car);
+
+?>
